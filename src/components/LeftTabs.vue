c60b228783ddecc7d1f576ee7e6433e0222db29f
blackmiaool <blackmiaool@gmail.com>
2017-12-03 18:07:05 +0800

    add github link

--- src/components/LeftTabs.vue
+++ src/components/LeftTabs.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="left-tabs-wrap">
-    
+
         <div class="avatar">
             <img v-if="avatar" :src="avatar" alt="" :class="{active:logged}">
         </div>
         <div class="tabs">
-    
+
             <router-link to="/todo" data-tab-name="groups" class="clickable">
                 <i class="fa fa-user-circle"></i>
             </router-link>
@@ -15,15 +15,18 @@
             <router-link to="/list" data-tab-name="projects" class="clickable">
                 <i class="fa fa-cubes"></i>
             </router-link>
-    
+
             <!--<router-link to="/settings" data-tab-name="settings" class="clickable">
-                                                                <i class="fa fa-cog"></i>
-                                                            </router-link>-->
+                                                                                        <i class="fa fa-cog"></i>
+                                                                                    </router-link>-->
             <!--                <div data-tab-name="search" class="clickable"></div>-->
             <!--                <div data-tab-name="other" class="clickable"></div>-->
             <router-link to="/login" data-tab-name="login" class="clickable">
                 <i class="fa fa-power-off"></i>
             </router-link>
+            <a href="https://github.com/blackmiaool/todo10" target="_blank" class="go-to-github" title="star this repo on github">
+                <i class="fa fa-github"></i>
+            </a>
         </div>
         <div class="tools">
             <div data-tool-name="logging"></div>
